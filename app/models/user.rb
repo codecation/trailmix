855b4b7c0412bdd4fd12278b9f03d72bbef4eb1a
Chris Hunt <c@chrishunt.co>
2014-09-24 14:42:01 -0600

    Remove Devise registration routes
    We want to make our own landing and settings pages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   has_many :entries
 
