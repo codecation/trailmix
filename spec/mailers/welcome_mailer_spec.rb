510669691161f21ea632f732e993b114ee59b738
Ben Orenstein <ben.orenstein@gmail.com>
2014-09-24 14:08:05 -0600

    Change default from address

--- spec/mailers/welcome_mailer_spec.rb
+++ spec/mailers/welcome_mailer_spec.rb
@@ -2,6 +2,7 @@ describe WelcomeMailer do
   describe ".welcome"  do
     it "welcomes the user" do
       user = create(:user)
+
       mail = WelcomeMailer.welcome(user)
 
       expect(mail.body.encoded).to include("Welcome")
