d9ac461b4c36652941bcc33c933ba78816e7f682
Chris Hunt <c@chrishunt.co>
2014-09-24 22:14:28 -0600

    Add Entry.newest for sorting by newest

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Entry, :type => :model do
+  describe ".newest" do
+    it "sorts entries with the newest first" do
+      user = create(:user)
+
+      entries = [
+        create(:entry, user: user, created_at: Time.zone.now),
+        create(:entry, user: user, created_at: 1.day.ago),
+        create(:entry, user: user, created_at: 2.days.ago)
+      ]
+
+      expect(user.entries.newest).to eq(entries)
+    end
+  end
 end
