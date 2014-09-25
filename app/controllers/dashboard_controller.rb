5b4dfccfe9d6423a53cef6f62acc5f356565d967
Chris Hunt <c@chrishunt.co>
2014-09-24 19:27:05 -0600

    Add pagination to entries on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
     if user_signed_in?
-      @entries = current_user.entries
+      @entries = current_user.entries.page(params[:page])
     else
       redirect_to new_registration_path unless user_signed_in?
     end
