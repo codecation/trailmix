1aaeb4eb3863dd3f2b60392643c9514486a2df64
Ben Orenstein <ben.orenstein@gmail.com>
2014-09-24 20:22:54 -0600

    Create a page for imports and link to it from dash
    Just a skeleton for now.

---
+++ app/controllers/imports_controller.rb
@@ -0,0 +1,4 @@
+class ImportsController < ApplicationController
+  def new
+  end
+end
--- spec/features/user_imports_ohlife_entries_spec.rb
+++ spec/features/user_imports_ohlife_entries_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 feature "User imports their OhLife entries" do
-
   scenario "immediately after creating their account" do
     user = create(:user)
 
     login_as(user)
     visit dashboard_path
+    click_link "Import your OhLife entries"
 
-    puts page.body
+    expect(page).to have_content("OhLife Refugee Import Area")
   end
 end
