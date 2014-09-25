c6bfa7b047e07adb212cd6d5d9079d5ee2485d3c
Chris Hunt <c@chrishunt.co>
2014-09-24 18:31:22 -0600

    Show all journal entries on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   def index
-    redirect_to new_registration_path unless user_signed_in?
+    if user_signed_in?
+      @entries = current_user.entries
+    else
+      redirect_to new_registration_path unless user_signed_in?
+    end
   end
 end
--- spec/features/user_visits_dashboard_spec.rb
+++ spec/features/user_visits_dashboard_spec.rb
@@ -1,11 +1,26 @@
 feature "User visits dashboard" do
-  scenario "when signed in" do
+  scenario "when signed in with entries" do
+    user = create(:user)
+    create(:entry, user: user, body: "My first entry")
+    create(:entry, user: user, body: "My second entry")
+
+    login_as(user)
+
+    visit dashboard_path
+
+    expect(current_path).to eq dashboard_path
+    expect(page).to have_content("My first entry")
+    expect(page).to have_content("My second entry")
+  end
+
+  scenario "when signed in without entries" do
     user = create(:user)
 
     login_as(user)
     visit dashboard_path
 
     expect(current_path).to eq dashboard_path
+    expect(page).to have_content("You have no journal entries.")
   end
 
   scenario "when signed out" do
