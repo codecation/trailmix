e470424fd64c9ddcf418db62670843f5d436ce04
Chris Hunt <c@chrishunt.co>
2014-09-25 20:48:47 -0600

    Remove Paperclip config from Import

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,13 +1,4 @@
 class Import < ActiveRecord::Base
   has_many :entries
   belongs_to :user
-
-  has_attached_file :raw_file
-
-  validates_attachment(
-    :raw_file,
-    presence: true,
-    content_type: { content_type: ["text/plain"] },
-    size: { less_than: 2.megabytes }
-  )
 end
