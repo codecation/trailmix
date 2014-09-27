63e53c4dcbb3fdeeaaf8c651ebba24ba65901a40
Ben Orenstein <ben.orenstein@gmail.com>
2014-09-27 01:50:46 -0600

    Make carrierwave clean up after itself

--- spec/support/carrierwave.rb
+++ spec/support/carrierwave.rb
@@ -2,3 +2,9 @@ CarrierWave.configure do |config|
   config.storage = :file
   config.enable_processing = false
 end
+
+RSpec.configure do |config|
+  config.after(:all) do
+    FileUtils.rm_r(Dir.glob("public/uploads"))
+  end
+end
