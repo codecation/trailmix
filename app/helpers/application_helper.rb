553cb4484403f518aa1550583719fc6565aa81ea
Chris Hunt <c@chrishunt.co>
2014-09-27 23:05:28 -0700

    Show entry dates in user time zone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def in_user_time_zone(time)
+    user_signed_in? ? time.in_time_zone(current_user.time_zone) : time
+  end
 end
--- spec/features/user_changes_timezone_spec.rb
+++ spec/features/user_changes_timezone_spec.rb
@@ -1,20 +1,25 @@
 feature "User changes timezone" do
-  scenario "from default to Arizona timezone" do
-    user = create(:user)
+  scenario "from Pacific Time to Melbourne" do
+    user = create(:user, time_zone: "Pacific Time (US & Canada)")
+    entry = create(:entry, user: user, created_at: "2014-01-01 00:00:00")
     login_as(user)
 
+    visit dashboard_path
+    expect(page).to have_content("Tuesday")
+
     visit edit_settings_path
-    select "(GMT-07:00) Arizona", from: :user_time_zone
+    select "(GMT+10:00) Melbourne", from: :user_time_zone
     click_button "Save"
 
     expect(page).to have_content("settings have been saved")
     expect(current_path).to eq dashboard_path
+    expect(page).to have_content("Wednesday")
 
     click_link "Settings"
 
     expect(page).to have_select(
       :user_time_zone,
-      selected: "(GMT-07:00) Arizona"
+      selected: "(GMT+10:00) Melbourne"
     )
   end
 
