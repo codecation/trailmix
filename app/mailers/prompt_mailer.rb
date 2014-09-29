ed51584c1c9ced0f4fcc38a68fd7dd84bedec801
Chris Hunt <c@chrishunt.co>
2014-09-28 20:39:46 -0700

    Move today into private method

--- app/mailers/prompt_mailer.rb
+++ app/mailers/prompt_mailer.rb
@@ -1,14 +1,16 @@
 class PromptMailer < ActionMailer::Base
   def prompt(user, entry)
     @entry = entry
-    today  = I18n.l(
-      Time.zone.now.in_time_zone(user.time_zone),
-      format: :for_prompt
-    )
 
     mail(
       to: user.email,
-      subject: "It's #{today}. How was your day?"
+      subject: "It's #{today(user)}. How was your day?"
     )
+  end
+
+  private
+
+  def today(user)
+    I18n.l(Time.zone.now.in_time_zone(user.time_zone), format: :for_prompt)
   end
 end
