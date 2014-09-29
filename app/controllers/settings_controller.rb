fb09bf07dcdfffffcb247715eab272bba6fcf7b1
Chris Hunt <c@chrishunt.co>
2014-09-29 08:26:22 -0700

    Extract user and utc offsets to methods

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,22 +4,32 @@ class SettingsController < ApplicationController
   before_filter :authenticate_user!
 
   def edit
-    offset = ActiveSupport::TimeZone[current_user.time_zone].utc_offset / 1.hour
-    @prompt_delivery_hour = current_user.prompt_delivery_hour + offset
+    @prompt_delivery_hour = user_prompt_delivery_hour
   end
 
   def update
-    time_zone = params[:user][:time_zone]
-    offset = ActiveSupport::TimeZone[time_zone].utc_offset / 1.hour
-    prompt_delivery_hour = params[:date][:prompt_delivery_hour].to_i - offset
-
     current_user.update_attributes(
-      time_zone: time_zone,
-      prompt_delivery_hour: prompt_delivery_hour
+      time_zone: params[:user][:time_zone],
+      prompt_delivery_hour: utc_prompt_delivery_hour
     )
 
     flash[:notice] = "Your settings have been saved."
 
     redirect_to dashboard_path
+  end
+
+  private
+
+  def user_prompt_delivery_hour
+    current_user.prompt_delivery_hour + offset(current_user.time_zone)
+  end
+
+  def utc_prompt_delivery_hour
+    params[:date][:prompt_delivery_hour].to_i -
+    offset(params[:user][:time_zone])
+  end
+
+  def offset(time_zone)
+    ActiveSupport::TimeZone[time_zone].utc_offset / 1.hour
   end
 end
