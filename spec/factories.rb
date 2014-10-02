77bbbc3f92375764fe2b78f618ee393fcdc6d37d
Chris Hunt <c@chrishunt.co>
2014-10-02 15:12:10 -0700

    Use double quotes to make @houndci happy

--- spec/factories.rb
+++ spec/factories.rb
@@ -17,14 +17,14 @@ FactoryGirl.define do
 
   factory :griddler_email, class: OpenStruct do
     to [{
-      full: 'to_user@example.com',
-      email: 'to_user@example.com',
-      token: 'to_user',
-      host: 'example.com',
+      full: "to_user@example.com",
+      email: "to_user@example.com",
+      token: "to_user",
+      host: "example.com",
       name: nil
     }]
-    from [{ email: 'from_user@example.com' }]
-    subject 'Hello Trailmix'
-    body 'Today was great'
+    from [{ email: "from_user@example.com" }]
+    subject "Hello Trailmix"
+    body "Today was great"
   end
 end
