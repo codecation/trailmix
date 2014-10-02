9defab14bc9b5337226655c0d6a75a82eb65a932
Ben Orenstein <ben.orenstein@gmail.com>
2014-10-02 17:44:45 -0400

    Synchronize schema.rb
    Looks like Rails wants to format the schema this way.

--- db/schema.rb
+++ db/schema.rb
@@ -32,18 +32,18 @@ ActiveRecord::Schema.define(version: 20140928181424) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",                           null: false
+    t.string   "encrypted_password",     default: "",                           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,                            null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                                    null: false
+    t.datetime "updated_at",                                                    null: false
     t.string   "time_zone",              default: "Central Time (US & Canada)", null: false
     t.integer  "prompt_delivery_hour",   default: 2,                            null: false
   end
