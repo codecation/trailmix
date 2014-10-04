1fbec76d5b73e7eb3db050b5e51eeeed5f77d5cb
Chris Hunt <c@chrishunt.co>
2014-10-04 13:53:43 -0700

    Remove day checking in settings spec
    We don't need to check this anymore because we're no longer doing time
    zone conversions. An entry has a date and that's all folks.

--- spec/features/user_changes_settings_spec.rb
+++ spec/features/user_changes_settings_spec.rb
@@ -1,10 +1,9 @@
 feature "User changes settings" do
   scenario "time zone from Pacific Time to Melbourne" do
     user = create(:user, time_zone: "Pacific Time (US & Canada)")
-    create(:entry, user: user, created_at: "2014-01-01 00:00:00")
+    create(:entry, user: user, date: Time.utc(2014, 1, 1))
     login_as(user)
     visit dashboard_path
-    expect(page).to have_content("Tuesday")
 
     click_link "Settings"
     select "Melbourne", from: :user_time_zone
@@ -12,7 +11,6 @@ feature "User changes settings" do
 
     expect(page).to have_content("settings have been saved")
     expect(current_path).to eq dashboard_path
-    expect(page).to have_content("Wednesday")
   end
 
   scenario "email delivery time from 9PM to 6AM" do
