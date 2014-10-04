95692cf55fce41b6251333d98a1faf5f6dfe1804
Chris Hunt <c@chrishunt.co>
2014-10-04 00:03:13 -0700

    Support old emails to today@ at the same time
    There are probably some folks that will take a day or so to reply to
    older emails. We should support both until we don't see anyone hitting
    today@trailmix.life.

--- spec/features/user_responds_to_prompt_spec.rb
+++ spec/features/user_responds_to_prompt_spec.rb
@@ -9,8 +9,16 @@ feature "User responds to a prompt" do
     expect(user.reload.entries).not_to be_empty
   end
 
-  def simulate_email_from(user)
-    post("/email_processor", email_params(user))
+  scenario "and it was sent to today@trailmix.life" do
+    user = create(:user, entries: [])
+
+    simulate_email_from(user, to: "today@trailmix.life")
+
+    expect(user.reload.entries).not_to be_empty
+  end
+
+  def simulate_email_from(user, options = {})
+    post("/email_processor", email_params(user).merge(options))
   end
 
   def email_params(user)
@@ -18,7 +26,7 @@ feature "User responds to a prompt" do
       headers: "Received: by 127.0.0.1 with SMTP...",
       to: user.reply_email,
       cc: "CC <cc@example.com>",
-      from: "whocares@example.com",
+      from: user.email,
       subject: "hello there",
       text: "this is an email message",
       html: "<p>this is an email message</p>",
