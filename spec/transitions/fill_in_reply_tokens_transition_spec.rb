7248d8891ff14c6e1b4baa780ca7d9813f370c00
Chris Hunt <c@chrishunt.co>
2014-10-04 09:45:27 -0700

    Remove whitespace for @houndci

--- spec/transitions/fill_in_reply_tokens_transition_spec.rb
+++ spec/transitions/fill_in_reply_tokens_transition_spec.rb
@@ -1,6 +1,6 @@
 describe FillInReplyTokensTransition do
   it "creates a reply token for all users missing a reply token" do
-    users = [ create(:user), create(:user) ]
+    users = [create(:user), create(:user)]
     users.each { |user| user.update_attribute(:reply_token, nil) }
 
     FillInReplyTokensTransition.new.perform
