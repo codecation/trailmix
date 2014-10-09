9f827124464f18e9931483980eee692a99d08899
Chris Hunt <c@chrishunt.co>
2014-10-09 08:49:52 -0700

    Add welcome email delimiter to Griddler config

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,5 +1,8 @@
 Griddler.configure do |config|
   config.processor_class = EmailProcessor
-  config.reply_delimiter = PromptMailer::PROMPT_TEXT
+  config.reply_delimiter = [
+    PromptMailer::PROMPT_TEXT,
+    WelcomeMailer::START_OF_MESSAGE
+  ]
   config.email_service = :sendgrid
 end
