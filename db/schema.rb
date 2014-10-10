607b9dfc7e4bc56e8c9bf9dc6e58a87ffa9da698
Chris Hunt <c@chrishunt.co>
2014-10-09 18:10:42 -0700

    Add stripe_id column to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004203140) do
+ActiveRecord::Schema.define(version: 20141010010843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141004203140) do
     t.string   "time_zone",              default: "Central Time (US & Canada)", null: false
     t.integer  "prompt_delivery_hour",   default: 2,                            null: false
     t.string   "reply_token",                                                   null: false
+    t.string   "stripe_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
