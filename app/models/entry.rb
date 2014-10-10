3002cc3afaee47a65a5865155f263b9b0cc12de3
Chris Hunt <c@chrishunt.co>
2014-10-10 08:42:48 -0700

    Return newest entry by date, not created_at

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,11 @@ class Entry < ActiveRecord::Base
   belongs_to :import
   belongs_to :user
 
-  scope :newest, -> { order("date DESC") }
+  scope :by_date, -> { order("date DESC") }
+
+  def self.newest
+    by_date.first
+  end
 
   def for_today?
     date == Time.zone.now.in_time_zone(user.time_zone).to_date
