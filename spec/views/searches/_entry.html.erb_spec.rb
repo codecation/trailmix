46fc6c6629f81099d8f89b6f45a0e12c197ab494
Ben Orenstein <ben.orenstein@gmail.com>
2014-10-12 14:52:31 -0400

    Move partial
    We don't really have a canonical view for entries, since we render them
    differently in search results and on the dashboard.
    
    This commit moves the entries/_entry partial into searches/.

--- spec/views/searches/_entry.html.erb_spec.rb
+++ spec/views/searches/_entry.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe "entries/_entry" do
+describe "searches/_entry" do
   it "highlights the searched-for term" do
     user = build_stubbed(:user)
     allow(view).to receive(:current_user).and_return(user)
@@ -8,9 +8,8 @@ describe "entries/_entry" do
     search = double("search", term: "cat")
     assign(:search, search)
 
-    render "entries/entry", entry: entry
+    render "searches/entry", entry: entry
 
     expect(rendered).to have_css("mark", text: search.term)
   end
-
 end
