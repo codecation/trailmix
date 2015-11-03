3fc39c97dca196c031114c61ede670bfbdd9b28a
Dave Shaffer <dave.shaffer@gmail.com>
2015-11-02 21:02:05 -0500

    Reorder user password spec file, make scenario names more descriptive, add "no data" and "no password" edge cases

--- spec/features/user_changes_password_spec.rb
+++ spec/features/user_changes_password_spec.rb
@@ -1,22 +1,5 @@
 feature "User changes password" do
-
-  scenario "with bad info" do
-    user = create(:user)
-    login_as(user)
-    visit edit_settings_path
-    click_link "Change password"
-
-    fill_in :user_password, with: "asdgs"
-    fill_in :user_password_confirmation, with: "BananaM0nkey123"
-    fill_in :user_current_password, with: "sadfdsafs"
-    click_button "Update"
-
-    expect(page).to have_content("Please review the problems below:")
-    expect(current_path).to eq(user_registration_path(:user))
-
-  end
-
-  scenario "with correct info" do
+  scenario "when the passwords match" do
     user = create(:user)
     login_as(user)
     visit edit_settings_path
@@ -29,6 +12,46 @@ feature "User changes password" do
 
     expect(page).to have_content("Your account has been updated successfully.")
     expect(current_path).to eq(root_path)
+  end
 
+  scenario "when the passwords do not match" do
+    user = create(:user)
+    login_as(user)
+    visit edit_settings_path
+    click_link "Change password"
+
+    fill_in :user_password, with: "asdgs"
+    fill_in :user_password_confirmation, with: "BananaM0nkey123"
+    fill_in :user_current_password, with: "sadfdsafs"
+    click_button "Update"
+
+    expect(page).to have_content("doesn't match Password")
+    expect(current_path).to eq(user_registration_path(:user))
+  end
+
+  scenario "without entering their current password" do
+    user = create(:user)
+    login_as(user)
+    visit edit_settings_path
+    click_link "Change password"
+
+    fill_in :user_password, with: "BananaM0nkey123"
+    fill_in :user_password_confirmation, with: "BananaM0nkey123"
+    click_button "Update"
+
+    expect(page).to have_content("can't be blank")
+    expect(current_path).to eq(user_registration_path(:user))
+  end
+
+  scenario "without entering anything" do
+    user = create(:user)
+    login_as(user)
+    visit edit_settings_path
+    click_link "Change password"
+
+    click_button "Update"
+
+    expect(page).to have_content("Please review the problems below:")
+    expect(current_path).to eq(user_registration_path(:user))
   end
 end
