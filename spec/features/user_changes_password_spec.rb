47c2df74579d69f2833b3b7ea2fef50a80889a3c
Ben Orenstein <ben.orenstein@gmail.com>
2016-01-25 17:47:29 -0600

    Tweaks

--- spec/features/user_changes_password_spec.rb
+++ spec/features/user_changes_password_spec.rb
@@ -1,12 +1,11 @@
 feature "User changes password" do
   scenario "when the passwords match" do
     user = create(:user)
+
     login_as(user)
     visit edit_settings_path
-    click_link "Change password"
-
-    fill_in :user_password, with: "BananaM0nkey123"
-    fill_in :user_password_confirmation, with: "BananaM0nkey123"
+    click_link "Change email/password"
+    fill_in :user_password, with: "password"
     fill_in :user_current_password, with: "abc123"
     click_button "Update"
 
@@ -14,44 +13,15 @@ feature "User changes password" do
     expect(current_path).to eq(root_path)
   end
 
-  scenario "when the passwords do not match" do
-    user = create(:user)
-    login_as(user)
-    visit edit_settings_path
-    click_link "Change password"
-
-    fill_in :user_password, with: "asdgs"
-    fill_in :user_password_confirmation, with: "BananaM0nkey123"
-    fill_in :user_current_password, with: "sadfdsafs"
-    click_button "Update"
-
-    expect(page).to have_content("doesn't match Password")
-    expect(current_path).to eq(user_registration_path(:user))
-  end
-
   scenario "without entering their current password" do
     user = create(:user)
     login_as(user)
     visit edit_settings_path
-    click_link "Change password"
-
-    fill_in :user_password, with: "BananaM0nkey123"
-    fill_in :user_password_confirmation, with: "BananaM0nkey123"
+    click_link "Change email/password"
+    fill_in :user_password, with: "password"
     click_button "Update"
 
     expect(page).to have_content("can't be blank")
-    expect(current_path).to eq(user_registration_path(:user))
-  end
-
-  scenario "without entering anything" do
-    user = create(:user)
-    login_as(user)
-    visit edit_settings_path
-    click_link "Change password"
-
-    click_button "Update"
-
-    expect(page).to have_content("Please review the problems below:")
     expect(current_path).to eq(user_registration_path(:user))
   end
 end
