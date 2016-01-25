a0a858e401486073b0346de41ffd064bd22bd9b0
Ben Orenstein <ben.orenstein@gmail.com>
2016-01-25 11:08:44 -0600

    Put config in right place

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,13 +1,11 @@
 require_relative "production"
 
-config.middleware.delete(Rack::SslEnforcer)
-
 Mail.register_interceptor(
   RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
 )
 
 Rails.application.configure do
-  # ...
+  config.middleware.delete(Rack::SslEnforcer)
 
   config.action_mailer.default_url_options = { host: 'trailmix-staging.herokuapp.com' }
 end
