fd2d0f72fb75b64b8751a125936b9d8fe4152199
Nick Holden <nick.r.holden@gmail.com>
2025-02-13 10:35:29 -0700

    Upgrade to Rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Trailmix
   class Application < Rails::Application
-    config.load_defaults 7.1
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.2
 
     config.i18n.enforce_available_locales = true
 
