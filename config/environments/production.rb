933e26806545a53e6815bb17ea4a7b1de4f56a67
Nick Holden <nick.r.holden@gmail.com>
2025-02-17 07:30:19 -0700

    Disable dynamic asset compliation in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  # https://devcenter.heroku.com/articles/rails-4-asset-pipeline#caching
+  config.assets.compile = false
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
